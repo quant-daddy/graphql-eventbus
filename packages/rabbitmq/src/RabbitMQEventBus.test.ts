8c8b45af8190a6a6429f9318c4d28e27f4a2cee4
Suraj Keshri <skk2142@columbia.edu>
2022-01-16 20:16:52 -0500

    adds rabbitmq bus

---
+++ packages/rabbitmq/src/RabbitMQEventBus.test.ts
@@ -0,0 +1,66 @@
+import { buildSchema } from "graphql";
+import { RabbitMQEventBus } from "./RabbitMQEventBus";
+import gql from "graphql-tag";
+import wait from "waait";
+import { LoggingPlugin } from "graphql-eventbus";
+
+jest.setTimeout(20000);
+
+describe("RabbitMQEventBus", () => {
+  test("works", async () => {
+    const schema = buildSchema(`
+    type TestEvent {
+      id: ID!
+      name: String
+    }
+    type Query {
+      TestEvent: TestEvent!
+    }
+  `);
+    const cb = jest.fn();
+    const bus = new RabbitMQEventBus({
+      serviceName: "test-7",
+      plugins: [LoggingPlugin()],
+      publisher: {
+        schema,
+      },
+      subscriber: {
+        cb: async (...args) => {
+          return cb(...args);
+        },
+        queries: gql`
+          query TestEvent {
+            TestEvent {
+              id
+            }
+          }
+        `,
+        schema,
+      },
+    });
+    await bus.init();
+    // wait for consumer queur to be initialized
+    await wait(5000);
+    cb.mockClear();
+    await bus.publish({
+      topic: "TestEvent",
+      payload: { id: "1", name: "coolio" },
+    });
+    await wait(5000);
+    await bus.closePublisher();
+    await bus.closeConsumer();
+    // console.log(cb.mock.calls);
+    expect(cb).toBeCalledTimes(1);
+    expect(cb.mock.calls[0][0]).toMatchObject({
+      _fullData: {
+        id: "1",
+        name: "coolio",
+      },
+      metadata: {},
+      payload: {
+        id: "1",
+      },
+      topic: "TestEvent",
+    });
+  });
+});
