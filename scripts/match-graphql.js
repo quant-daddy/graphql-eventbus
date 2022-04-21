1506737399fb1b70dbae0561ee195996160822d7
Suraj Keshri <surajkeshri@gmail.com>
2022-04-21 17:41:25 -0400

    adds graphql versions for CI test

---
+++ scripts/match-graphql.js
@@ -0,0 +1,13 @@
+const { writeFileSync } = require("fs");
+const { resolve } = require("path");
+const { argv, cwd } = require("process");
+
+const pkgPath = resolve(cwd(), "./package.json");
+
+const pkg = require(pkgPath);
+
+const version = argv[2];
+
+pkg.devDependencies.graphql = version;
+
+writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), "utf8");
